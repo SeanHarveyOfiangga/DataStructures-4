d88ed69e1a1be1efa606d336222480645c4f5868
unknown <seanharveyofiangga@gmail.com>
2022-11-19 17:59:54 +0800

    Created the talk back function of harveyAI

--- HarveyAI.py
+++ HarveyAI.py
@@ -1,6 +1,16 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+engine.say("Hello, I am Harvey.")
+engine.say("What can I do for you?")
+engine.runAndWait()
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 try:
     with sr.Microphone() as source:
         print("Listening...")
@@ -8,6 +18,6 @@ try:
         command = listener.recognize_google(voice)
         command = command.lower()
         if 'harvey' in command:
-            print(command) 
+            talk(command) 
 except:
     pass
